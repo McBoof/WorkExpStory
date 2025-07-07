16d92ecb02d1faabfda5659cee5a9a749340ec0c
Jon Marks <jon@kaldorgroup.com>
2025-07-07 11:10:04 +0000

    Dad Joke

--- bots/jon_bot.py
+++ bots/jon_bot.py
@@ -1,10 +1,21 @@
+import requests
 from base_bot import BaseBot
-
 
 class JonBot(BaseBot):
 
     def __init__(self):
         super().__init__("Jon")
 
+    def get_dad_joke(self):
+        url = "https://icanhazdadjoke.com/"
+        headers = {
+            "Accept": "application/json",
+            "User-Agent": "JonBot (you@example.com)"  # customize this
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data["joke"]
+
     def get_sentence(self):
-        return f"Hello Jon Was Here"
+        return self.get_dad_joke()
