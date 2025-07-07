7de3b66f45d29cedd042c569e360f8e1f0f6caf0
zAnk Sincerely <zak1208@icloud.com>
2025-07-07 10:46:26 +0000

    Updated ZakBot(BaseBot)

--- bots/zak_bot.py
+++ bots/zak_bot.py
@@ -1,5 +1,5 @@
 from base_bot import BaseBot
-
+import time
 
 class ZakBot(BaseBot):
 
@@ -8,5 +8,7 @@ class ZakBot(BaseBot):
         super().__init__("Zak")
 
     def get_sentence(self):
+
+        time.sleep(2)
 
         return f"\nMy name is {self.name}. I made the code. It was difficult to get the repository to work. But unfortunately, something went wrong. Now I can't do anything but sing this stupid song!\n"
