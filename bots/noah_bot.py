c1510692d8b383c1bec73e9d5364d4c0f4cb5d32
Noah <noahkmarks@icloud.com>
2025-07-07 11:10:09 +0000

    International Space Station NoahBot

--- bots/noah_bot.py
+++ bots/noah_bot.py
@@ -1,12 +1,27 @@
-"""
-Noah's Bot Implementation
-Contributes one sentence to the collaborative story.
-"""
-
 from base_bot import BaseBot
+
+import requests
+
+
+def get_iss_location():
+    url = "http://api.open-notify.org/iss-now.json"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        position = data['iss_position']
+        latitude = position['latitude']
+        longitude = position['longitude']
+
+        return "the ISS is currently at: " + str(latitude) + str(longitude)
+
+
 class NoahBot(BaseBot):
     def __init__(self):
         super().__init__("Noah")
 
     def get_sentence(self):
-        return f"HEELLOO II AAMM {self.name}"
+        sentance = "I am NoahBot : "
+        sentance = sentance + str(get_iss_location())
+        return sentance
+
