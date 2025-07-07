017831a02c5d0e6eb00e972ad6e6282c5740e6b2
kate and willooooooeww <skmpeerless@gmail.com>
2025-07-07 10:18:44 +0000

    Change Willow Bot

--- bots/willow_bot.py
+++ bots/willow_bot.py
@@ -1,28 +1,12 @@
-"""
-Willow's Bot Implementation
-Contributes one sentence to the collaborative story.
-"""
 
 from base_bot import BaseBot
 
 class WillowBot(BaseBot):
-    """
-    Willow's storytelling bot.
-    Customize the get_sentence() method to contribute your unique sentence!
-    """
+   
     
     def __init__(self):
-        """Initialize Willow's bot."""
         super().__init__("Willow")
     
     def get_sentence(self):
-        """
-        Generate Willow's sentence for the story.
-        
-        TODO: Customize this method to return your creative sentence!
-        You can use self.name to reference your name in the sentence.
-        
-        Returns:
-            str: Willow's contribution to the story
-        """
+       
         return f"Hello {self.name}"
